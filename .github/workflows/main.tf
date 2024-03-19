5b935b6ba7e9f21920f04ae00df5a09db7e7387b
sarveshraval1998 <163859367+sarveshraval1998@users.noreply.github.com>
2024-03-19 12:46:21 +0530

    Update main.tf
    added confluent support

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -14,3 +14,37 @@ terraform {
     }
   }
 }
+locals {
+  cloud  = "AWS"
+  region = "us-east-2"
+}
+
+provider "confluent" {
+  cloud_api_key    = var.confluent_cloud_api_key
+  cloud_api_secret = var.confluent_cloud_api_secret
+}
+
+data "confluent_environment" "existing_env" {
+  display_name = "Dev"  
+}
+
+data "confluent_kafka_cluster" "existing_cluster" {
+  display_name = "DF_AWS_DEV" 
+
+  environment {
+    id = data.confluent_environment.existing_env.id
+  }
+}
+
+resource "confluent_kafka_cluster" "my_kafka_cluster" {
+  display_name = data.confluent_kafka_cluster.existing_cluster.display_name
+  availability = data.confluent_kafka_cluster.existing_cluster.availability
+  cloud        = local.cloud
+  region       = local.region
+  basic {}
+
+  environment {
+    id = data.confluent_kafka_cluster.existing_cluster.environment.id
+  }
+
+}
