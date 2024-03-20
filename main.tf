244ec7a2dfad1b0b0c280e7c4306b61f80d4d5f4
sarveshraval1998 <163859367+sarveshraval1998@users.noreply.github.com>
2024-03-20 18:18:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
   cloud {
-    organization = "flink_cccloud"
+    organization = "flink_cnewcloud"
 
     workspaces {
-      name = "cicd_flink_ccloud"
+      name = "cicd_flink_cnewcloud"
     }
   }
 
