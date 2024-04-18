7344d25b8d7fd21ec44b9955f24a670f0c5f6f9e
sarveshraval1998 <163859367+sarveshraval1998@users.noreply.github.com>
2024-04-18 16:18:53 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,13 +9,71 @@ variable "confluent_cloud_api_secret" {
   sensitive   = true
 }
 
-variable "confluent_schema_registry_api_key" {
-  description = "Confluent Schema Registry API Key"
+variable "confluent_cluster" {
+  description = "Confluent Cluster Name"
   type        = string
 }
 
-variable "confluent_schema_registry_api_secret" {
-  description = "Confluent Schema Registry API Secret"
+variable "confluent_environment" {
+  description = "Confluent Environment Name"
+  type        = string
+}
+
+variable "confluent_service_account" {
+  description = "Confluent Service Account"
+  type        = string
+}
+
+variable "confluent_sourcetopic" {
+  description = "Confluent Source Topic"
+  type        = string
+  sensitive   = true
+}
+
+variable "confluent_sinktopic" {
+  description = "Confluent Sink Topic"
+  type        = string
+  sensitive   = true
+}
+
+variable "confluent_connector" {
+  description = "Confluent Connector Name"
+  type        = string
+  sensitive   = true
+}
+
+variable "confluent_srcluster_id" {
+  description = "Confluent Schema Registry Cluster Id"
+  type        = string
+  sensitive   = true
+}
+
+variable "confluent_flink_endpoint" {
+  description = "Confluent Flink Endpoint"
+  type        = string
+  sensitive   = true
+}
+
+variable "confluent_cluster_keyid" {
+  description = "Confluent cluster key id"
+  type        = string
+  sensitive   = true
+}
+
+variable "confluent_cluster_keysecret" {
+  description = "Confluent cluster key secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "confluent_flink_keyid" {
+  description = "Confluent flink key id"
+  type        = string
+  sensitive   = true
+}
+
+variable "confluent_flink_keysecret" {
+  description = "Confluent flink key secret"
   type        = string
   sensitive   = true
 }
